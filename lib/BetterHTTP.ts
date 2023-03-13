d888a1c7a633b483c3a238fe9ac834801209fbfa
amogus-technologies <124402074+amogus-technologies@users.noreply.github.com>
2023-03-13 16:43:47 +0000

    commit message

--- lib/BetterHTTP.ts
+++ lib/BetterHTTP.ts
@@ -14,9 +14,11 @@ export interface Request {
 
 export class HTTP {
     constructor() {
-        this.parseRequest = this.parseRequest.bind(this);
+        this.parseRequest = HTTP.parseRequest;
+        this.parseResponse = HTTP.parseResponse;
     }
     public parseRequest: (request: Buffer) => Request;
+    public parseResponse: (response: Buffer) => Response;
     public static parseRequest(request: Buffer): Request {
         let requestStr = request.toString('binary');
         let [firstLine, ...head] = requestStr.split('\r\n\r\n')[0].split('\r\n');
@@ -32,5 +34,33 @@ export class HTTP {
             headers = { ...headers, ...header };
         }
         return { body, method, path, headers }
+    }
+    public static parseResponse(response: Buffer): Response {
+        let responseStr = response.toString('binary');
+        let [firstLine, ...head] = responseStr.split('\r\n\r\n')[0].split('\r\n');
+        let headersArr = head.map(header => {
+            let entry = header.split(': ');
+            return { [entry[0]]: entry[1] };
+        });
+        let bodyStart = responseStr.indexOf('\r\n\r\n') + 4;
+        let body = response.subarray(bodyStart);
+        let [_protocol, statusCode, statusMessage] = firstLine.split(' ');
+        let headers = {};
+        for(let header of headersArr) {
+            headers = { ...headers, ...header };
+        }
+        return { body, statusCode: parseInt(statusCode), statusMessage, headers }
+    }
+    public static serializeRequest(request: Request): Buffer {
+        let headers = Object.entries(request.headers).map(([key, value]) => `${key}: ${value}`).join('\r\n');
+        let requestStr = `${request.method} ${request.path} HTTP/1.1\r\n${headers}\r\n\r\n`;
+        var requestBuf = Buffer.from(requestStr, 'binary');
+        return Buffer.concat([requestBuf, request.body]);
+    }
+    public static serializeResponse(response: Response): Buffer {
+        let headers = Object.entries(response.headers).map(([key, value]) => `${key}: ${value}`).join('\r\n');
+        let responseStr = `HTTP/1.1 ${response.statusCode} ${response.statusMessage}\r\n${headers}\r\n\r\n`;
+        var responseBuf = Buffer.from(responseStr, 'binary');
+        return Buffer.concat([responseBuf, response.body]);
     }
 }
